109b43cf184161dedefd1ab002413315877d7559
Kvin <kevin.blenner@mindee.co>
2022-08-30 10:23:55 +0200

    feat: using a polygon in base field

--- src/main/java/com/mindee/model/fields/BaseField.java
+++ src/main/java/com/mindee/model/fields/BaseField.java
@@ -1,5 +1,7 @@
 package com.mindee.model.fields;
 
+import com.mindee.geometry.Polygon;
+import com.mindee.geometry.PolygonUtils;
 import com.mindee.utils.GeometryUtils;
 import java.util.List;
 import lombok.Getter;
@@ -10,17 +12,16 @@ public abstract class BaseField {
   private final Boolean reconstructed;
   private final String rawValue;
   private final Double confidence;
-  private final List<List<Double>> polygon;
+  private final Polygon polygon;
   private final List<List<Double>> bbox;
   private final Integer page;
-
 
   protected BaseField(Boolean reconstructed, String rawValue, Double confidence,
       List<List<Double>> polygon, Integer page) {
     this.reconstructed = reconstructed;
     this.rawValue = rawValue;
     this.confidence = confidence;
-    this.polygon = polygon;
+    this.polygon = polygon != null ? PolygonUtils.getFrom(polygon) : null;
     this.page = page;
     if (polygon != null && polygon.size() > 0) {
       this.bbox = GeometryUtils.deriveBBoxFromPolygon(polygon);
