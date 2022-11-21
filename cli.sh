f928273e3cb54afab4f1bcf02ec2cc18eed5fd53
ianaré <97107275+ianardee@users.noreply.github.com>
2022-11-21 16:54:00 +0100

    :bug: fixes and updates for CLI tool (#27)

--- cli.sh
+++ cli.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env sh
+
 set -e
 
 if ! ls ./target/libs/picocli-* 2>&1 >/dev/null; then
     mvn dependency:copy-dependencies -DoutputDirectory=target/libs -Dhttps.protocols=TLSv1.2
 fi
 
-java -cp "target/mindee-api-java-1.0-SNAPSHOT.jar:target/libs/*" com.mindee.CLI CLI "$@"
+java -cp "target/mindee-api-java-1.1.1.jar:target/libs/*" com.mindee.CLI CLI "$@"
