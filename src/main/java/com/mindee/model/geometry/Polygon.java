14315ea1ac8735cd7d13192b06f313b38d393a57
Kvin <kevin.blenner@mindee.co>
2022-12-20 17:45:16 +0100

    fix: revert Polygon changes

--- src/main/java/com/mindee/model/geometry/Polygon.java
+++ src/main/java/com/mindee/model/geometry/Polygon.java
@@ -2,16 +2,17 @@ package com.mindee.model.geometry;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.mindee.model.deserialization.PolygonDeserializer;
-
 import java.util.ArrayList;
 import java.util.List;
-
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@JsonDeserialize(using = PolygonDeserializer.class)
 public class Polygon {
-  private List<Point> coordinates;
+  private List<Point> coordinates = new ArrayList<>();
 
+  @Builder
   public Polygon(List<Point> coordinates) {
     this.coordinates = coordinates;
   }
