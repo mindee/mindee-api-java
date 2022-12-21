e37537631404ac2b205f6ca024ff98ba21333b8f
Kvin <kevin.blenner@mindee.co>
2022-12-21 19:34:01 +0100

    wip: new mindee client

--- src/main/java/com/mindee/MindeeClient.java
+++ src/main/java/com/mindee/MindeeClient.java
@@ -1,2 +1,36 @@
-package com.mindee;public class MindeeClient {
+package com.mindee;
+
+import com.mindee.parsing.MindeeApi;
+import com.mindee.parsing.common.Document;
+import com.mindee.parsing.common.Inference;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MindeeClient {
+
+  private final MindeeApi mindeeApi;
+
+  public MindeeClient() {
+    this.mindeeApi = new MindeeApi();
+  }
+
+  public MindeeClient(String apiKey) {
+    this.mindeeApi = new MindeeApi(apiKey);
+  }
+
+  public <T extends Inference> Document<T> parse(
+    Class<T> type,
+    InputStream fileStream,
+    String fileName,
+    boolean includeWords) throws IOException {
+
+      return this.mindeeApi.predict(
+        type,
+        ParseParameter.builder()
+          .fileStream(fileStream)
+          .fileName(fileName)
+          .includeWords(includeWords)
+          .build());
+  }
 }
