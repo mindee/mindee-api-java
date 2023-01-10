ff822b6e577e9de62e81036385484dfc43019aff
Kvin <kevin.blenner@mindee.co>
2023-01-10 11:31:16 +0100

    :sparkles: support Carte vitale V1

---
+++ src/main/java/com/mindee/parsing/fr/cartevitale/CarteVitaleV1DocumentPrediction.java
@@ -0,0 +1,55 @@
+package com.mindee.parsing.fr.cartevitale;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.mindee.parsing.SummaryHelper;
+import com.mindee.parsing.common.field.DateField;
+import com.mindee.parsing.common.field.StringField;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@EqualsAndHashCode
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CarteVitaleV1DocumentPrediction {
+
+  /**
+   * The list of the names of the person.
+   */
+  @JsonProperty("given_names")
+  private List<StringField> givenNames = new ArrayList<>();
+  /**
+   * The surname of the person.
+   */
+  @JsonProperty("surname")
+  private StringField surname;
+  /**
+   * The social security number.
+   */
+  @JsonProperty("social_security")
+  private StringField socialSecurityNumber;
+  /**
+   * Date the check was issued.
+   */
+  @JsonProperty("issuance_date")
+  private DateField issuanceDate;
+
+  @Override
+  public String toString() {
+
+    String summary =
+        String.format(":Given names: %s%n",
+          this.getGivenNames().stream()
+          .map(StringField::toString)
+          .collect(Collectors.joining(", "))) +
+        String.format(":Surname: %s%n", this.getSurname()) +
+        String.format(":ID Number: %s%n", this.getSocialSecurityNumber()) +
+        String.format(":Issuance date: %s%n", this.getIssuanceDate());
+
+    return SummaryHelper.cleanSummary(summary);
+  }
+}
