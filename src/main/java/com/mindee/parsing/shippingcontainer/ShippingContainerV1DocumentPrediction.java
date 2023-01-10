9d7810c47b4dfb38dc7e803060b9d82627ac4ec2
Kvin <kevin.blenner@mindee.co>
2023-01-10 11:30:46 +0100

    :sparkles: support Shipping container V1

---
+++ src/main/java/com/mindee/parsing/shippingcontainer/ShippingContainerV1DocumentPrediction.java
@@ -0,0 +1,41 @@
+package com.mindee.parsing.shippingcontainer;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.mindee.parsing.SummaryHelper;
+import com.mindee.parsing.common.field.StringField;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ShippingContainerV1DocumentPrediction {
+
+  /**
+   * ISO 6346 code for container owner prefix + equipment identifier.
+   */
+  @JsonProperty("owner")
+  private StringField owner;
+  /**
+   * ISO 6346 code for container serial number (6+1 digits).
+   */
+  @JsonProperty("serial_number")
+  private StringField serialNumber;
+  /**
+   * ISO 6346 code for container length, height and type.
+   */
+  @JsonProperty("size_type")
+  private StringField sizeType;
+
+  @Override
+  public String toString() {
+
+    String summary =
+        String.format(":Owner: %s%n", this.getOwner()) +
+        String.format(":Serial number: %s%n", this.getSerialNumber()) +
+        String.format(":Size and type: %s%n", this.getSizeType());
+
+    return SummaryHelper.cleanSummary(summary);
+  }
+}
---
+++ src/main/java/com/mindee/parsing/shippingcontainer/ShippingContainerV1Inference.java
@@ -0,0 +1,17 @@
+package com.mindee.parsing.shippingcontainer;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.mindee.parsing.EndpointInfo;
+import com.mindee.parsing.common.Inference;
+import lombok.Getter;
+
+/**
+ * The shipping container V1 inference.
+ */
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+@EndpointInfo(endpointName = "shipping_containers", version = "1")
+public class ShippingContainerV1Inference
+  extends Inference<ShippingContainerV1DocumentPrediction, ShippingContainerV1DocumentPrediction> {
+
+}
