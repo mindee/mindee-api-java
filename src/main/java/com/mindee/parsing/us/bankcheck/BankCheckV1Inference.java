4282c4e501192883ab4fb936e4f6954e04997663
Kvin <kevin.blenner@mindee.co>
2023-01-10 11:32:41 +0100

    :sparkles: support US Bank check V1

---
+++ src/main/java/com/mindee/parsing/us/bankcheck/BankCheckV1Inference.java
@@ -0,0 +1,17 @@
+package com.mindee.parsing.us.bankcheck;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.mindee.parsing.EndpointInfo;
+import com.mindee.parsing.common.Inference;
+import lombok.Getter;
+
+/**
+ * The bank check V1 inference.
+ */
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+@EndpointInfo(endpointName = "shipping_containers", version = "1")
+public class BankCheckV1Inference
+  extends Inference<BankCheckV1DocumentPrediction, BankCheckV1DocumentPrediction> {
+
+}
