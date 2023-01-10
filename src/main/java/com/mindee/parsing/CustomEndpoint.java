83b6bf9edde40c3452cf37bcfdb840d8b631000b
Kvin <kevin.blenner@mindee.co>
2023-01-10 15:18:13 +0100

    :sparkles: version 1 is selected by default for custom document

--- src/main/java/com/mindee/parsing/CustomEndpoint.java
+++ src/main/java/com/mindee/parsing/CustomEndpoint.java
@@ -1,24 +1,33 @@
 package com.mindee.parsing;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public final class CustomEndpoint {
 
   /**
    * The name of the product associated to the expected model.
    */
-  private String endpointName;
+  private final String endpointName;
 
   /**
    * The name of the account that owns the API. Useful when using custom builder.
    */
-  private String accountName;
+  private final String accountName;
 
   /**
    * The version number of the API. Without the v (for example for the v1.2: 1.2).
    */
-  private String version;
+  private String version = "1";
+
+  public CustomEndpoint(String endpointName, String accountName) {
+    this.endpointName = endpointName;
+    this.accountName = accountName;
+  }
+
+  public CustomEndpoint(String endpointName, String accountName, String version) {
+    this.endpointName = endpointName;
+    this.accountName = accountName;
+    this.version = version;
+  }
 }
