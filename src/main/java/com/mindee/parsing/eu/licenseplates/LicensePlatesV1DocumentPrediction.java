d0ae91642a2b0a23ad77cbef16df516475702847
Kvin <kevin.blenner@mindee.co>
2023-01-10 11:47:52 +0100

    :sparkles: support EU License plates V1

---
+++ src/main/java/com/mindee/parsing/eu/licenseplates/LicensePlatesV1DocumentPrediction.java
@@ -0,0 +1,36 @@
+package com.mindee.parsing.eu.licenseplates;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.mindee.parsing.SummaryHelper;
+import com.mindee.parsing.common.field.StringField;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@EqualsAndHashCode
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class LicensePlatesV1DocumentPrediction {
+
+  /**
+   * A list of license plates values.
+   */
+  @JsonProperty("license_plates")
+  private List<StringField> licensePlates = new ArrayList<>();
+
+  @Override
+  public String toString() {
+
+    String summary =
+        String.format(":License plates: %s%n",
+          this.getLicensePlates().stream()
+            .map(StringField::toString)
+            .collect(Collectors.joining(", ")));
+
+    return SummaryHelper.cleanSummary(summary);
+  }
+}
