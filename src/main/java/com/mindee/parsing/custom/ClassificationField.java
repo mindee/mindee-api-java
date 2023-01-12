6360d27a987ad493ae508ae86b92d3c41f0d8263
Kvin <kevin.blenner@mindee.co>
2023-01-12 15:20:49 +0100

    :sparkles: separate classification fields and other fields in Custom V1

---
+++ src/main/java/com/mindee/parsing/custom/ClassificationField.java
@@ -0,0 +1,29 @@
+package com.mindee.parsing.custom;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ClassificationField {
+
+  /**
+   * The confidence about the zone of the value extracted.
+   * A value from 0 to 1.
+   */
+  @JsonProperty("confidence")
+  private double confidence;
+  /**
+   * The content of the value.
+   */
+  @JsonProperty("value")
+  private String value;
+
+  @Override
+  public String toString() {
+    return value == null ? "" : value;
+  }
+}
--- src/main/java/com/mindee/parsing/custom/CustomV1DocumentPrediction.java
+++ src/main/java/com/mindee/parsing/custom/CustomV1DocumentPrediction.java
@@ -1,24 +1,40 @@
 package com.mindee.parsing.custom;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.mindee.parsing.SummaryHelper;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.HashMap;
-import java.util.TreeMap;
+import java.util.*;
 
+@AllArgsConstructor
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class CustomV1DocumentPrediction extends HashMap<String, ListField> {
+@JsonDeserialize(using = CustomV1DocumentPredictionDeserializer.class)
+public class CustomV1DocumentPrediction {
+
+  /**
+   * Classification fields.
+   */
+  private Map<String, ClassificationField> classificationFields;
+
+  /**
+   * Fields that are not classifications.
+   */
+  private Map<String, ListField> fields;
 
   @Override
   public String toString() {
 
-    TreeMap<String, ListField> sorted = new TreeMap<>(this);
+    TreeMap<String, ListField> sorted = new TreeMap<>(fields);
 
     StringBuilder summary = new StringBuilder();
 
-    for (Entry<String, ListField> entry : sorted.entrySet()) {
+    for (Map.Entry<String, ClassificationField> entry : classificationFields.entrySet()) {
+      summary.append(String.format(":%s: %s%n", entry.getKey(), entry.getValue()));
+    }
+    for (Map.Entry<String, ListField> entry : sorted.entrySet()) {
       summary.append(String.format(":%s: %s%n", entry.getKey(), entry.getValue()));
     }
 
