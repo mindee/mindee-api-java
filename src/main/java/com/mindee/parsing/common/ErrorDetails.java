a107828c68b5674f8f1ff01ef218e67db0ce5d88
Kvin <kevin.blenner@mindee.co>
2023-02-02 11:45:16 +0100

    :recycle:! using IllegalStateException instead of NotSerializableException

--- src/main/java/com/mindee/parsing/common/ErrorDetails.java
+++ src/main/java/com/mindee/parsing/common/ErrorDetails.java
@@ -1,6 +1,5 @@
 package com.mindee.parsing.common;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Getter;
