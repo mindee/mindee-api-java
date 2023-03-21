dbdc1af4288cb931ad4930fcf29e07db83b32c93
Ianaré Sévi <ianare@mindee.co>
2023-03-21 17:35:08 +0100

    :white_check_mark: add tests for environment variables

--- src/test/java/com/mindee/MindeeSettingsTest.java
+++ src/test/java/com/mindee/MindeeSettingsTest.java
@@ -3,13 +3,26 @@ package com.mindee;
 import junit.framework.TestCase;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.ClearEnvironmentVariable;
+import org.junitpioneer.jupiter.SetEnvironmentVariable;
 
 public class MindeeSettingsTest extends TestCase {
 
   @Test
-  void givenAClientWithMissingConfigured_whenParsed_thenShouldThrowException() {
+  @ClearEnvironmentVariable(key = "MINDEE_API_KEY")
+  @ClearEnvironmentVariable(key = "MINDEE_API_URL")
+  void clearedEnvironmentVariablesAndEmptyParams() {
     Assertions.assertThrows(
       IllegalArgumentException.class,
       () -> new MindeeSettings("", ""));
+  }
+
+  @Test
+  @SetEnvironmentVariable(key = "MINDEE_API_KEY", value = "abcd")
+  @SetEnvironmentVariable(key = "MINDEE_API_URL", value = "https://example.com")
+  void setEnvironmentVariablesAndEmptyParams() {
+    MindeeSettings settings = new MindeeSettings("", "");
+    Assertions.assertEquals(settings.getApiKey(), "abcd");
+    Assertions.assertEquals(settings.getBaseUrl(), "https://example.com");
   }
 }
