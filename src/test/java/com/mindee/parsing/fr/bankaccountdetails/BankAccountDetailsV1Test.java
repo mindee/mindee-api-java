aef052252ff7c6e278872f077beb1235fedd68fd
Ianaré Sévi <ianare@mindee.co>
2023-04-24 12:02:26 +0200

    :sparkles: add support for FR bank account details

---
+++ src/test/java/com/mindee/parsing/fr/bankaccountdetails/BankAccountDetailsV1Test.java
@@ -0,0 +1,40 @@
+package com.mindee.parsing.fr.bankaccountdetails;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mindee.parsing.common.PredictResponse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class BankAccountDetailsV1Test {
+
+  @Test
+  void givenBankAccountDetailsV1_whenDeserialized_MustHaveAValidSummary() throws IOException {
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.findAndRegisterModules();
+
+    JavaType type = objectMapper.getTypeFactory().constructParametricType(
+      PredictResponse.class,
+      BankAccountDetailsV1Inference.class
+    );
+    PredictResponse<BankAccountDetailsV1Inference> prediction = objectMapper.readValue(
+      new File("src/test/resources/data/fr/bank_account_details/response_v1/complete.json"),
+      type
+    );
+
+    String[] actualLines = prediction.getDocument().toString().split(System.lineSeparator());
+    List<String> expectedLines = Files.readAllLines(
+      Paths.get("src/test/resources/data/fr/bank_account_details/response_v1/summary_full.rst")
+    );
+    String expectedSummary = String.join(String.format("%n"), expectedLines);
+    String actualSummary = String.join(String.format("%n"), actualLines);
+
+    Assertions.assertEquals(expectedSummary, actualSummary);
+  }
+}
