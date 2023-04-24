6af77983385421f361495cae92e67aaf241598bd
Ianaré Sévi <ianare@mindee.co>
2023-04-24 12:02:26 +0200

    :sparkles: add support for FR bank account details

---
+++ src/main/java/com/mindee/parsing/fr/bankaccountdetails/BankAccountDetailsV1Inference.java
@@ -0,0 +1,16 @@
+package com.mindee.parsing.fr.bankaccountdetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.mindee.parsing.EndpointInfo;
+import com.mindee.parsing.common.Inference;
+import lombok.Getter;
+
+/**
+* The definition for bank_account_details v1.
+*/
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+@EndpointInfo(endpointName = "bank_account_details", version = "1")
+public class BankAccountDetailsV1Inference
+    extends Inference<BankAccountDetailsV1DocumentPrediction, BankAccountDetailsV1DocumentPrediction> {
+}
