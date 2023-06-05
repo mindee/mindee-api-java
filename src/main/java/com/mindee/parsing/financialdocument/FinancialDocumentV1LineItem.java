da4657bdb7ce47ddbd82544f88e97ba6322994be
Ianaré Sévi <ianare@mindee.co>
2023-06-05 12:15:56 +0200

    :sparkles: add support for financial document v1.1

--- src/main/java/com/mindee/parsing/financialdocument/FinancialDocumentV1LineItem.java
+++ src/main/java/com/mindee/parsing/financialdocument/FinancialDocumentV1LineItem.java
@@ -3,84 +3,108 @@ package com.mindee.parsing.financialdocument;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.mindee.parsing.SummaryHelper;
-import com.mindee.parsing.common.field.BaseField;
+import com.mindee.parsing.common.field.LineItemField;
+import java.util.HashMap;
 import lombok.Getter;
 
-
 /**
- * Line items details.
+ * List of line item details.
  */
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class FinancialDocumentV1LineItem extends BaseField {
-  @JsonProperty("product_code")
-  private String productCode;
+public class FinancialDocumentV1LineItem extends LineItemField {
+
+  /**
+   * The item description.
+   */
   @JsonProperty("description")
-  private String description;
+  String description;
+  /**
+   * The product code referring to the item.
+   */
+  @JsonProperty("product_code")
+  String productCode;
+  /**
+   * The item quantity
+   */
   @JsonProperty("quantity")
-  private Double quantity;
-  @JsonProperty("unit_price")
-  private Double unitPrice;
-  @JsonProperty("total_amount")
-  private Double totalAmount;
-  @JsonProperty("tax_rate")
-  private Double taxRate;
+  Double quantity;
+  /**
+   * The item tax amount.
+   */
   @JsonProperty("tax_amount")
-  private Double taxAmount;
+  Double taxAmount;
+  /**
+   * The item tax rate in percentage.
+   */
+  @JsonProperty("tax_rate")
+  Double taxRate;
+  /**
+   * The item total amount.
+   */
+  @JsonProperty("total_amount")
+  Double totalAmount;
+  /**
+   * The item unit price.
+   */
+  @JsonProperty("unit_price")
+  Double unitPrice;
 
   /**
    * Output the line in a format suitable for inclusion in an rST table.
    */
   public String toTableLine() {
-    String[] printable = this.printableValues();
-    String taxRate = !printable[5].equals("") ? " (" + printable[5] + "%)" : "";
-    return "| "
-      + String.format("%-20s", printable[0])
-      + " | "
-      + String.format("%-7s", printable[1])
-      + " | "
-      + String.format("%-7s", printable[2])
-      + " | "
-      + String.format("%-8s", printable[3])
-      + " | "
-      + String.format("%-16s", printable[4] + taxRate)
-      + " | "
-      + String.format("%-36s", printable[6])
-      + " |";
-  }
-
-  protected String[] printableValues() {
-    String descriptionSummary = (this.description != null ? this.description : "");
-    if (descriptionSummary.length() > 33) {
-      descriptionSummary = descriptionSummary.substring(0, 33) + "...";
-    }
-    return new String[]{
-      this.productCode != null ? this.productCode : "",
-      this.quantity != null ? SummaryHelper.formatAmount(this.quantity) : "",
-      this.unitPrice != null ? SummaryHelper.formatAmount(this.unitPrice) : "",
-      this.totalAmount != null ? SummaryHelper.formatAmount(this.totalAmount) : "",
-      this.taxAmount != null ? SummaryHelper.formatAmount(this.taxAmount) : "",
-      this.taxRate != null ? SummaryHelper.formatAmount(this.taxRate) : "",
-      this.description != null ? descriptionSummary : "",
-    };
+    HashMap<String, String> printable = this.printableValues();
+    return String.format("| %-36s ", printable.get("description"))
+      + String.format("| %-12s ", printable.get("productCode"))
+      + String.format("| %-8s ", printable.get("quantity"))
+      + String.format("| %-10s ", printable.get("taxAmount"))
+      + String.format("| %-12s ", printable.get("taxRate"))
+      + String.format("| %-12s ", printable.get("totalAmount"))
+      + String.format("| %-10s |", printable.get("unitPrice"));
   }
 
   @Override
   public String toString() {
-    String[] printable = this.printableValues();
-    return "Product Code: "
-      + printable[0]
-      + ", Quantity: "
-      + printable[1]
-      + ", Unit Price: "
-      + printable[2]
-      + ", Total Amount: "
-      + printable[3]
-      + ", Tax Amount: "
-      + printable[4]
-      + ", Tax Rate (%): "
-      + printable[5]
-      + ", Description: "
-      + printable[6].trim();
+    HashMap<String, String> printable = this.printableValues();
+    return String.format("Description: %s", printable.get("description"))
+      + String.format("Product code: %s", printable.get("productCode"))
+      + String.format("Quantity: %s", printable.get("quantity"))
+      + String.format("Tax Amount: %s", printable.get("taxAmount"))
+      + String.format("Tax Rate (%%): %s", printable.get("taxRate"))
+      + String.format("Total Amount: %s", printable.get("totalAmount"))
+      + String.format("Unit Price: %s", printable.get("unitPrice"));
+  }
+
+  protected HashMap<String, String> printableValues() {
+    HashMap<String, String> printable = new HashMap<>();
+
+    String descriptionSummary = (this.description != null ? this.description : "");
+    if (descriptionSummary.length() > 33) {
+      descriptionSummary = descriptionSummary.substring(0, 33) + "...";
+    }
+    printable.put("description", descriptionSummary);
+    printable.put("productCode", this.productCode != null ? this.productCode : "");
+    printable.put(
+        "quantity",
+        this.quantity != null ? SummaryHelper.formatAmount(this.quantity) : ""
+    );
+    printable.put(
+        "taxAmount",
+        this.taxAmount != null ? SummaryHelper.formatAmount(this.taxAmount) : ""
+    );
+    printable.put(
+        "taxRate",
+        this.taxRate != null ? SummaryHelper.formatAmount(this.taxRate) : ""
+    );
+    printable.put(
+        "totalAmount",
+        this.totalAmount != null ? SummaryHelper.formatAmount(this.totalAmount) : ""
+    );
+    printable.put(
+        "unitPrice",
+        this.unitPrice != null ? SummaryHelper.formatAmount(this.unitPrice) : ""
+    );
+    return printable;
   }
 }
