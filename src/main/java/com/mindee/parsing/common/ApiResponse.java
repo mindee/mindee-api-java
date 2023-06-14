f1ab3ba0759eae3284d7fe6dbd16303857a1171c
Ianaré Sévi <ianare@mindee.co>
2023-06-14 18:43:03 +0200

    :bug: proper handling and testing of timezones in dates

---
+++ src/main/java/com/mindee/parsing/common/ApiResponse.java
@@ -0,0 +1,21 @@
+package com.mindee.parsing.common;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Optional;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Base class for all responses from the Mindee API.
+ */
+@Data
+@EqualsAndHashCode
+@JsonIgnoreProperties(ignoreUnknown = true)
+abstract public class ApiResponse {
+  /**
+   * Information from Mindee about the api request.
+   */
+  @JsonProperty("api_request")
+  ApiRequest apiRequest;
+}
