98bae4b652fec5a189d25c98fa163186858c3780
ianaré <97107275+ianardee@users.noreply.github.com>
2023-06-20 14:18:07 +0200

    :sparkles: add support for FR bank account details v2 (#106)

---
+++ src/main/java/com/mindee/product/fr/bankaccountdetails/BankAccountDetailsV2.java
@@ -0,0 +1,16 @@
+package com.mindee.product.fr.bankaccountdetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.mindee.http.EndpointInfo;
+import com.mindee.parsing.common.Inference;
+import lombok.Getter;
+
+/**
+ * The definition for bank_account_details v2.
+ */
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+@EndpointInfo(endpointName = "bank_account_details", version = "2")
+public class BankAccountDetailsV2
+    extends Inference<BankAccountDetailsV2Document, BankAccountDetailsV2Document> {
+}
