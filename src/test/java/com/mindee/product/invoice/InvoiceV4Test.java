67480f702b440a307e1fe38dcc4ee6c774ae4cf3
ianaré <97107275+ianardee@users.noreply.github.com>
2023-07-24 23:09:40 +0200

    :white_check_mark: add unit tests for receipt v4, invoice v4 (#110)

--- src/test/java/com/mindee/product/invoice/InvoiceV4Test.java
+++ src/test/java/com/mindee/product/invoice/InvoiceV4Test.java
@@ -2,36 +2,61 @@ package com.mindee.product.invoice;
 
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mindee.parsing.common.Document;
+import com.mindee.parsing.common.Page;
 import com.mindee.parsing.common.PredictResponse;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-class InvoiceV4Test {
+/**
+ * Unit tests for InvoiceV4.
+ */
+public class InvoiceV4Test {
 
-  @Test
-  void givenAnInvoiceV4_whenDeserialized_MustHaveAValidSummary() throws IOException {
-
+  protected PredictResponse<InvoiceV4> getPrediction() throws IOException {
     ObjectMapper objectMapper = new ObjectMapper();
     objectMapper.findAndRegisterModules();
 
-    JavaType type = objectMapper.getTypeFactory().constructParametricType(PredictResponse.class,
-        InvoiceV4.class);
-    PredictResponse<InvoiceV4> invoiceV4Prediction = objectMapper.readValue(
-        new File("src/test/resources/invoice/response_v4/complete.json"),
-        type);
+    JavaType type = objectMapper.getTypeFactory().constructParametricType(
+      PredictResponse.class,
+      InvoiceV4.class
+    );
+    return objectMapper.readValue(
+      new File("src/test/resources/invoice/response_v4/complete.json"),
+      type
+    );
+  }
 
-    String[] actualLines = invoiceV4Prediction.getDocument().toString().split(System.lineSeparator());
-    List<String> expectedLines = Files
-        .readAllLines(Paths.get("src/test/resources/invoice/response_v4/summary_full.rst"));
+  @Test
+  void whenCompleteDeserialized_mustHaveValidDocumentSummary() throws IOException {
+    PredictResponse<InvoiceV4> prediction = getPrediction();
+    Document<InvoiceV4> doc = prediction.getDocument();
+    String[] actualLines = doc.toString().split(System.lineSeparator());
+    List<String> expectedLines = Files.readAllLines(
+      Paths.get("src/test/resources/invoice/response_v4/summary_full.rst")
+    );
     String expectedSummary = String.join(String.format("%n"), expectedLines);
     String actualSummary = String.join(String.format("%n"), actualLines);
 
     Assertions.assertEquals(expectedSummary, actualSummary);
   }
 
+  @Test
+  void whenCompleteDeserialized_mustHaveValidPage0Summary() throws IOException {
+    PredictResponse<InvoiceV4> prediction = getPrediction();
+    Page<InvoiceV4Document> page = prediction.getDocument().getInference().getPages().get(0);
+    String[] actualLines = page.toString().split(System.lineSeparator());
+    List<String> expectedLines = Files.readAllLines(
+      Paths.get("src/test/resources/invoice/response_v4/summary_page0.rst")
+    );
+    String expectedSummary = String.join(String.format("%n"), expectedLines);
+    String actualSummary = String.join(String.format("%n"), actualLines);
+
+    Assertions.assertEquals(expectedSummary, actualSummary);
+  }
 }
