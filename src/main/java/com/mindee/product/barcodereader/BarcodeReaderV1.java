a1009632d02acdbf428c6c3e0c4ba8f3dc6c33c0
ianaré <97107275+ianardee@users.noreply.github.com>
2023-09-06 17:12:26 +0200

    :sparkles: add barcode reader v1 (#114)

---
+++ src/main/java/com/mindee/product/barcodereader/BarcodeReaderV1.java
@@ -0,0 +1,16 @@
+package com.mindee.product.barcodereader;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.mindee.http.EndpointInfo;
+import com.mindee.parsing.common.Inference;
+import lombok.Getter;
+
+/**
+ * The definition for Barcode Reader, API version 1.
+ */
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+@EndpointInfo(endpointName = "barcode_reader", version = "1")
+public class BarcodeReaderV1
+    extends Inference<BarcodeReaderV1Page, BarcodeReaderV1Document> {
+}
---
+++ src/main/java/com/mindee/product/barcodereader/BarcodeReaderV1Page.java
@@ -0,0 +1,51 @@
+package com.mindee.product.barcodereader;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.mindee.parsing.SummaryHelper;
+import com.mindee.parsing.standard.StringField;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+/**
+ * Page data for Barcode Reader, API version 1.
+ */
+@Getter
+@EqualsAndHashCode(callSuper = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class BarcodeReaderV1Page extends BarcodeReaderV1Document {
+
+  /**
+   * List of decoded 1D barcodes.
+   */
+  @JsonProperty("codes_1d")
+  private List<StringField> codes1D = new ArrayList<>();
+  /**
+   * List of decoded 2D barcodes.
+   */
+  @JsonProperty("codes_2d")
+  private List<StringField> codes2D = new ArrayList<>();
+
+  @Override
+  public String toString() {
+    StringBuilder outStr = new StringBuilder();
+    String codes1D = SummaryHelper.arrayToString(
+        this.getCodes1D(),
+        "%n              "
+    );
+    outStr.append(
+        String.format(":Barcodes 1D: %s%n", codes1D)
+    );
+    String codes2D = SummaryHelper.arrayToString(
+        this.getCodes2D(),
+        "%n              "
+    );
+    outStr.append(
+        String.format(":Barcodes 2D: %s%n", codes2D)
+    );
+    outStr.append(super.toString());
+    return SummaryHelper.cleanSummary(outStr.toString());
+  }
+}
