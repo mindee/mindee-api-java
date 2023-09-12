979416c292a3984a772cdca7b13f4f179f953b01
Ianaré Sévi <ianare@mindee.co>
2023-09-12 16:20:11 +0200

    :sparkles: add error code in HTTP exceptions

--- src/main/java/com/mindee/http/MindeeHttpException.java
+++ src/main/java/com/mindee/http/MindeeHttpException.java
@@ -10,11 +10,15 @@ import lombok.Getter;
 public class MindeeHttpException extends MindeeException {
   /** Standard HTTP status code. */
   private final int statusCode;
+  /** Error details. */
   private final String details;
+  /** Error code (not HTTP code). */
+  private final String code;
 
-  public MindeeHttpException(int statusCode, String message, String details) {
+  public MindeeHttpException(int statusCode, String message, String details, String code) {
     super(message);
     this.statusCode = statusCode;
     this.details = details;
+    this.code = code;
   }
 }
