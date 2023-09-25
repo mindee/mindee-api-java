3749f971681db9e92d1a6026de5ad1b03b945ea9
Ianaré Sévi <ianare@mindee.co>
2023-09-25 13:09:16 +0200

    :sparkles: add a method to simplify getting the document object

--- src/test/java/com/mindee/product/invoicesplitter/InvoiceSplitterV1Test.java
+++ src/test/java/com/mindee/product/invoicesplitter/InvoiceSplitterV1Test.java
@@ -2,7 +2,7 @@ package com.mindee.product.invoicesplitter;
 
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mindee.parsing.common.PredictResponse;
+import com.mindee.parsing.common.AsyncPredictResponse;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -12,19 +12,33 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class InvoiceSplitterV1Test {
-  @Test
-  void givenAnInvoiceSplitterResponse_whenDeserialized_MustHaveAValidSummary() throws IOException {
 
+  protected AsyncPredictResponse<InvoiceSplitterV1> getPrediction(String name) throws IOException {
     ObjectMapper objectMapper = new ObjectMapper();
     objectMapper.findAndRegisterModules();
 
-    JavaType type = objectMapper.getTypeFactory().constructParametricType(PredictResponse.class,
-        InvoiceSplitterV1.class);
-    PredictResponse<InvoiceSplitterV1> splitterPrediction = objectMapper.readValue(
-        new File("src/test/resources/products/invoice_splitter/response_v1/complete.json"),
-        type);
+    JavaType type = objectMapper.getTypeFactory().constructParametricType(
+      AsyncPredictResponse.class,
+      InvoiceSplitterV1.class
+    );
+    return objectMapper.readValue(
+      new File("src/test/resources/products/invoice_splitter/response_v1/" + name + ".json"),
+      type
+    );
+  }
 
-    String[] actualLines = splitterPrediction.getDocument().toString().split(System.lineSeparator());
+  @Test
+  void whenEmptyDeserialized_mustHaveValidProperties() throws IOException {
+    AsyncPredictResponse<InvoiceSplitterV1> response = getPrediction("empty");
+    InvoiceSplitterV1Document docPrediction = response.getDocumentObj().getInference().getPrediction();
+    Assertions.assertTrue(docPrediction.getInvoicePageGroups().isEmpty());
+  }
+
+  @Test
+  void givenAnInvoiceSplitterResponse_whenDeserialized_MustHaveAValidSummary() throws IOException {
+    AsyncPredictResponse<InvoiceSplitterV1> response = getPrediction("complete");
+
+    String[] actualLines = response.getDocumentObj().toString().split(System.lineSeparator());
     List<String> expectedLines = Files
         .readAllLines(Paths.get("src/test/resources/products/invoice_splitter/response_v1/summary_full.rst"));
     String expectedSummary = String.join(String.format("%n"), expectedLines);
