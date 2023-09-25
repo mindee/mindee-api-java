d71764689b480861794507f3dc65d2870cb00746
Vivek <vivek@mindee.co>
2023-09-25 13:47:24 -0230

    modifying bbox

--- src/main/java/com/mindee/geometry/Bbox.java
+++ src/main/java/com/mindee/geometry/Bbox.java
@@ -1,33 +1,31 @@
 package com.mindee.geometry;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
  * True representation of a mathematical bounding box.
  */
 @Getter
+@AllArgsConstructor
 public final class Bbox {
 
+  /**
+   * minX The minimum X coordinate.
+   */
   private final double minX;
-  private final double minY;
+  /**
+   * The maximum X coordinate.
+   */
   private final double maxX;
-  private final double maxY;
+  /**
+   * The minimum Y coordinate.
+   */
+  private final double minY;
 
   /**
-   * The default constructor.
-   *
-   * @param minX The minimum X coordinate.
-   *
-   * @param maxX The maximum X coordinate.
-   *
-   * @param minY The minimal Y coordinate.
-   *
-   * @param maxY The maximum Y coordinate.
+   * The maximum Y coordinate.
    */
-  public Bbox(double minX, double maxX, double minY, double maxY) {
-    this.minX = minX;
-    this.minY = minY;
-    this.maxX = maxX;
-    this.maxY = maxY;
-  }
+  private final double maxY;
+  
 }
