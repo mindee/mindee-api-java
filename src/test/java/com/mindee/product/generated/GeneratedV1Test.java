23909de9fc2362926ac41c4a897217dc5833343b
ianaré sévi <ianare@mindee.co>
2024-03-14 12:26:11 +0100

    compiles

--- src/test/java/com/mindee/product/generated/GeneratedV1Test.java
+++ src/test/java/com/mindee/product/generated/GeneratedV1Test.java
@@ -1,5 +1,27 @@
 package com.mindee.product.generated;
 
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mindee.parsing.common.AsyncPredictResponse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
 public class GeneratedV1Test {
-  protected 
+  protected AsyncPredictResponse<GeneratedV1> getAsyncPrediction(String name) throws IOException {
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.findAndRegisterModules();
+
+    JavaType type = objectMapper.getTypeFactory().constructParametricType(
+      AsyncPredictResponse.class,
+      GeneratedV1.class
+    );
+    return objectMapper.readValue(
+      new File("src/test/resources/products/generated/response_v1/" + name + ".json"),
+      type
+    );
+  }
+
 }
