9980a8ff4945d0cba83fcbc7d6e05af470b30ad3
ianaré sévi <ianare@mindee.co>
2024-05-21 18:58:51 +0200

    :sparkles: allow sending multiple anchors when building line items

--- src/test/java/com/mindee/parsing/custom/lineitems/FakeListField.java
+++ src/test/java/com/mindee/parsing/custom/lineitems/FakeListField.java
@@ -12,6 +12,89 @@ class FakeListField {
   private FakeListField() {
   }
 
+  static Map<String, ListField> get2completeLines1halfLine() {
+    Map<String, ListField> fakes = new HashMap<>();
+    fakes.put(
+      "birthDates",
+      new ListField(
+        1.0,
+        Arrays.asList(
+          new ListFieldValue(
+            "1986-10-23",
+            1.0,
+            new Polygon(
+              Arrays.asList(
+                new Point(0.818, 0.398),
+                new Point(0.902, 0.398),
+                new Point(0.902, 0.406),
+                new Point(0.818, 0.406)))),
+          new ListFieldValue(
+            "2012-02-13",
+            1.0,
+            new Polygon(
+              Arrays.asList(
+                new Point(0.819, 0.442),
+                new Point(0.902, 0.442),
+                new Point(0.902, 0.451),
+                new Point(0.819, 0.451)))),
+          new ListFieldValue(
+            "1895-02-28",
+            1.0,
+            new Polygon(
+              Arrays.asList(
+                new Point(0.819, 0.462),
+                new Point(0.902, 0.462),
+                new Point(0.902, 0.471),
+                new Point(0.819, 0.471))))
+        ))
+    );
+    fakes.put(
+      "names",
+      new ListField(
+        1.0,
+        Arrays.asList(
+          new ListFieldValue(
+            "Kevin",
+            1.0,
+            new Polygon(
+              Arrays.asList(
+                new Point(0.082, 0.398),
+                new Point(0.144, 0.398),
+                new Point(0.144, 0.407),
+                new Point(0.082, 0.407)))),
+          new ListFieldValue(
+            "Mindee",
+            1.0,
+            new Polygon(
+              Arrays.asList(
+                new Point(0.152, 0.398),
+                new Point(0.222, 0.398),
+                new Point(0.222, 0.407),
+                new Point(0.152, 0.407)))),
+          new ListFieldValue(
+            "Ianaré",
+            1.0,
+            new Polygon(
+              Arrays.asList(
+                new Point(0.081, 0.442),
+                new Point(0.15, 0.442),
+                new Point(0.15, 0.451),
+                new Point(0.081, 0.451)))),
+          new ListFieldValue(
+            "Mindee",
+            1.0,
+            new Polygon(
+              Arrays.asList(
+                new Point(0.157, 0.442),
+                new Point(0.26, 0.442),
+                new Point(0.26, 0.451),
+                new Point(0.157, 0.451))))
+        ))
+    );
+
+    return fakes;
+  }
+
   static Map<String, ListField> getWith2ValuesByExpectedLines() {
     Map<String, ListField> fakes = new HashMap<>();
     fakes.put(
@@ -36,7 +119,8 @@ class FakeListField {
                 new Point(0.819, 0.442),
                 new Point(0.902, 0.442),
                 new Point(0.902, 0.451),
-                new Point(0.819, 0.451))))))
+                new Point(0.819, 0.451))))
+        ))
     );
 
     fakes.put(
@@ -79,7 +163,8 @@ class FakeListField {
                         new Point(0.157, 0.442),
                         new Point(0.26, 0.442),
                         new Point(0.26, 0.451),
-                        new Point(0.157, 0.451))))))
+                        new Point(0.157, 0.451))))
+        ))
     );
 
     return fakes;
@@ -110,7 +195,8 @@ class FakeListField {
                         new Point(0.819, 0.442),
                         new Point(0.902, 0.442),
                         new Point(0.902, 0.451),
-                        new Point(0.819, 0.451))))))
+                        new Point(0.819, 0.451))))
+        ))
     );
 
     fakes.put(
@@ -162,7 +248,8 @@ class FakeListField {
                         new Point(0.082, 0.486),
                         new Point(0.151, 0.486),
                         new Point(0.151, 0.495),
-                        new Point(0.082, 0.495))))))
+                        new Point(0.082, 0.495))))
+        ))
     );
 
     return fakes;
@@ -170,7 +257,6 @@ class FakeListField {
 
   static Map<String, ListField> getWith1ExpectedLines() {
     Map<String, ListField> fakes = new HashMap<>();
-
     fakes.put(
       "birthDates",
       new ListField(
@@ -184,9 +270,9 @@ class FakeListField {
                           new Point(0.818, 0.398),
                           new Point(0.902, 0.398),
                           new Point(0.902, 0.406),
-                          new Point(0.818, 0.406))))))
+                          new Point(0.818, 0.406))))
+          ))
     );
-
     fakes.put(
       "names",
       new ListField(
@@ -209,15 +295,14 @@ class FakeListField {
                         new Point(0.152, 0.398),
                         new Point(0.222, 0.398),
                         new Point(0.222, 0.407),
-                        new Point(0.152, 0.407))))))
+                        new Point(0.152, 0.407))))
+        ))
     );
-
     return fakes;
   }
 
-  static Map<String, ListField>  getWithPolygonsNotExactlyOnTheSameAxis() {
+  static Map<String, ListField> getWithPolygonsNotExactlyOnTheSameAxis() {
     Map<String, ListField> fakes = new HashMap<>();
-
     fakes.put(
       "birthDates",
       new ListField(
@@ -240,9 +325,9 @@ class FakeListField {
                         new Point(0.581, 0.45),
                         new Point(0.656, 0.45),
                         new Point(0.656, 0.458),
-                        new Point(0.581, 0.458))))))
+                        new Point(0.581, 0.458))))
+        ))
     );
-
     fakes.put(
       "names",
       new ListField(
@@ -274,15 +359,14 @@ class FakeListField {
                         new Point(0.118, 0.45),
                         new Point(0.169, 0.451),
                         new Point(0.169, 0.458),
-                        new Point(0.117, 0.457))))))
+                        new Point(0.117, 0.457))))
+        ))
     );
-
     return fakes;
   }
 
   static Map<String, ListField> getSampleWichRender2LinesInsteadOfOne() {
     Map<String, ListField> fakes = new HashMap<>();
-
     fakes.put(
       "names",
       new ListField(
@@ -332,9 +416,9 @@ class FakeListField {
                         new Point(0.263, 0.42),
                         new Point(0.33, 0.42),
                         new Point(0.33, 0.428),
-                        new Point(0.263, 0.428))))))
+                        new Point(0.263, 0.428))))
+        ))
     );
-
     return fakes;
   }
 }
