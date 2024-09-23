0da469c80092b65650b118ad797102dfcfa26ddf
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2024-09-23 14:41:06 +0200

    update invoice splitter integration test to rely on levenshtein distance

--- src/test/java/com/mindee/TestingUtilities.java
+++ src/test/java/com/mindee/TestingUtilities.java
@@ -1,6 +1,14 @@
 package com.mindee;
 
+import java.util.Objects;
+
 public class TestingUtilities {
+  /**
+   * Retrieves the version from an RST prediction output.
+   *
+   * @param rstStr An RST prediction output string.
+   * @return The version.
+   */
   public static String getVersion(String rstStr) {
     int versionLineStartPos = rstStr.indexOf(":Product: ");
     int versionEndPos = rstStr.indexOf("\n", versionLineStartPos);
@@ -11,6 +19,12 @@ public class TestingUtilities {
     return substring.substring(versionStartPos + 2);
   }
 
+  /**
+   * Retrieves an ID from an RST prediction output string.
+   *
+   * @param rstStr An RST prediction output string.
+   * @return The ID.
+   */
   public static String getId(String rstStr) {
     int idStartPos = rstStr.indexOf(":Mindee ID: ") + 12;
     int idEndPos = rstStr.indexOf("\n:Filename:");
@@ -18,11 +32,89 @@ public class TestingUtilities {
     return rstStr.substring(idStartPos, idEndPos);
   }
 
+  /**
+   * Retrieves a filename from an RST prediction output string.
+   *
+   * @param rstStr An RST output string.
+   * @return The filename.
+   */
   public static String getFileName(String rstStr) {
     int idStartPos = rstStr.indexOf(":Filename: ") + 11;
     int idEndPos = rstStr.indexOf("\n\nInference");
 
     return rstStr.substring(idStartPos, idEndPos);
   }
+
+  /**
+   * Compute the Levenshtein distance between two strings.
+   * Taken & adapted from <a href="https://rosettacode.org/wiki/Levenshtein_distance#Iterative_space_optimized_(even_bounded)">here</a>
+   *
+   * @param refStr Source string to compare.
+   * @param targetStr Target string to compare.
+   * @return The Levenshtein distance between the two strings.
+   */
+  private static int levenshteinDistance(String refStr, String targetStr) {
+    if (Objects.equals(refStr, targetStr)) {
+      return 0;
+    }
+    int sourceLength = refStr.length();
+    int targetLength = targetStr.length();
+    if (sourceLength == 0) {
+      return targetLength;
+    }
+    if (targetLength == 0) {
+      return sourceLength;
+    }
+    if (sourceLength < targetLength) {
+      int tempLength = sourceLength;
+      sourceLength = targetLength;
+      targetLength = tempLength;
+      String tempString = refStr;
+      refStr = targetStr;
+      targetStr = tempString;
+    }
+
+    int[] distanceVector = new int[targetLength + 1];
+    for (int i = 0; i <= targetLength; i++) {
+      distanceVector[i] = i;
+    }
+
+    for (int i = 1; i <= sourceLength; i++) {
+      distanceVector[0] = i;
+      int previousDistance = i - 1;
+      int minDistance = previousDistance;
+      for (int j = 1; j <= targetLength; j++) {
+        int currentDistance =
+          previousDistance + (refStr.charAt(i - 1) == targetStr.charAt(j - 1) ? 0 : 1);
+        distanceVector[j] =
+          Math.min(Math.min(1 + (previousDistance = distanceVector[j]), 1 + distanceVector[j - 1]),
+            currentDistance);
+        if (previousDistance < minDistance) {
+          minDistance = previousDistance;
+        }
+      }
+    }
+    return distanceVector[targetLength];
+  }
+
+  /**
+   * Computes the Levenshtein ratio between two given strings.
+   *
+   * @param referenceString First string.
+   * @param targetString    Second string.
+   * @return The ratio of similarities between the two strings.
+   */
+  public static double levenshteinRatio(String referenceString, String targetString) {
+    int referenceLength = referenceString.length();
+    int targetLength = targetString.length();
+    int maxLength = Math.max(referenceLength, targetLength);
+
+    if (referenceLength == 0 && targetLength == 0) {
+      return 1.0;
+    }
+
+    return 1.0 - (double) levenshteinDistance(referenceString, targetString) / maxLength;
+  }
+
 }
 
--- src/test/java/com/mindee/extraction/InvoiceSplitterAutoExtractionIT.java
+++ src/test/java/com/mindee/extraction/InvoiceSplitterAutoExtractionIT.java
@@ -1,5 +1,7 @@
 package com.mindee.extraction;
 
+import static com.mindee.TestingUtilities.levenshteinRatio;
+
 import com.mindee.MindeeClient;
 import com.mindee.MindeeException;
 import com.mindee.TestingUtilities;
@@ -10,7 +12,6 @@ import com.mindee.parsing.common.PredictResponse;
 import com.mindee.product.invoice.InvoiceV4;
 import com.mindee.product.invoicesplitter.InvoiceSplitterV1;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -70,15 +71,28 @@ public class InvoiceSplitterAutoExtractionIT {
     Assertions.assertEquals(2, extractedPDFsStrict.size());
     Assertions.assertEquals("default_sample_001-001.pdf", extractedPDFsStrict.get(0).getFilename());
     Assertions.assertEquals("default_sample_002-002.pdf", extractedPDFsStrict.get(1).getFilename());
-    PredictResponse<InvoiceV4> invoice0 = getInvoicePrediction(extractedPDFsStrict.get(0).asInputSource());
+    PredictResponse<InvoiceV4> invoice0 =
+      getInvoicePrediction(extractedPDFsStrict.get(0).asInputSource());
 
-    String testStringRSTInvoice0 = prepareInvoiceReturn("src/test/resources/products/invoices/response_v4/summary_full_invoice_p1.rst", invoice0.getDocument());
-    Assertions.assertEquals(testStringRSTInvoice0, String.join(String.format("%n"), invoice0.getDocument().toString().split(System.lineSeparator())));
+    String testStringRSTInvoice0 = prepareInvoiceReturn(
+      "src/test/resources/products/invoices/response_v4/summary_full_invoice_p1.rst",
+      invoice0.getDocument());
+    Assertions.assertEquals(testStringRSTInvoice0, String.join(String.format("%n"),
+      invoice0.getDocument().toString().split(System.lineSeparator())));
 
-    PredictResponse<InvoiceV4> invoice1 = getInvoicePrediction(extractedPDFsStrict.get(1).asInputSource());
+    PredictResponse<InvoiceV4> invoice1 =
+      getInvoicePrediction(extractedPDFsStrict.get(1).asInputSource());
 
-    String testStringRSTInvoice1 = prepareInvoiceReturn("src/test/resources/products/invoices/response_v4/summary_full_invoice_p2.rst", invoice1.getDocument());
-    Assertions.assertEquals(testStringRSTInvoice1, String.join(String.format("%n"), invoice1.getDocument().toString().split(System.lineSeparator())));
+    String testStringRSTInvoice1 = prepareInvoiceReturn(
+      "src/test/resources/products/invoices/response_v4/summary_full_invoice_p2.rst",
+      invoice1.getDocument());
+    Assertions.assertTrue(
+      levenshteinRatio(
+        testStringRSTInvoice1,
+        String.join(String.format("%n"),
+          invoice1.getDocument().toString().split(System.lineSeparator())
+        )
+      ) > 0.97);
 
 
   }
