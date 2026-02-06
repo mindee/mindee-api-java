458c455fd8599e50e08c0f4858de2b678c642ed0
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2026-02-06 16:52:23 +0100

    fix lint + test

--- src/test/java/com/mindee/MindeeSettingsTest.java
+++ src/test/java/com/mindee/MindeeSettingsTest.java
@@ -2,19 +2,16 @@ package com.mindee;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledForJreRange;
-import org.junit.jupiter.api.condition.JRE;
 import org.junitpioneer.jupiter.SetEnvironmentVariable;
 
 public class MindeeSettingsTest {
 
   @Test
-  @DisabledForJreRange(min = JRE.JAVA_9, disabledReason = "JUnit Pioneer environment variable modification requires additional JVM flags on Java 9+")
   @SetEnvironmentVariable(key = "MINDEE_API_KEY", value = "abcd")
   @SetEnvironmentVariable(key = "MINDEE_API_URL", value = "https://example.com")
   void setEnvironmentVariablesAndEmptyParams() {
     MindeeSettings settings = new MindeeSettings("", "");
-    Assertions.assertEquals(settings.getApiKey().orElse(""), "abcd");
-    Assertions.assertEquals(settings.getBaseUrl(), "https://example.com");
+    Assertions.assertEquals("abcd", settings.getApiKey().orElse(""));
+    Assertions.assertEquals("https://example.com", settings.getBaseUrl());
   }
 }
