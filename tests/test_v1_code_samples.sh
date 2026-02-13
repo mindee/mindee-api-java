cab4e50904b930407f03a9154e659c4f127a2452
ianaré <97107275+ianardee@users.noreply.github.com>
2026-02-13 11:02:30 +0100

    :recycle: split test script in v1 and v2 (#295)

--- tests/test_v1_code_samples.sh
+++ tests/test_v1_code_samples.sh
@@ -1,12 +1,9 @@
 #! /bin/sh
 set -e
 
-OUTPUT_FILE='SimpleMindeeClient.java'
+OUTPUT_FILE='SimpleMindeeClientV1.java'
 ACCOUNT=$1
 ENDPOINT=$2
-API_KEY=$3
-API_KEY_V2=$4
-MODEL_ID=$5
 
 if [ -z "${ACCOUNT}" ]; then echo "ACCOUNT is required"; exit 1; fi
 if [ -z "${ENDPOINT}" ]; then echo "ENDPOINT is required"; exit 1; fi
@@ -14,21 +11,17 @@ if [ -z "${ENDPOINT}" ]; then echo "ENDPOINT is required"; exit 1; fi
 # We need the dependencies otherwise we get class not found exceptions
 mvn dependency:copy-dependencies
 
-for f in $(find docs/code_samples -maxdepth 1 -name "*.txt" -not -name "workflow_execution.txt" | sort -h)
+for f in $(
+  find docs/code_samples -maxdepth 1 -name "*.txt" -not -name "workflow_*.txt" -not -name "v2_*.txt" | sort -h
+)
 do
-  if echo "${f}" | grep -q "default_v2.txt"; then
-    if [ -z "${API_KEY_V2}" ] || [ -z "${MODEL_ID}" ]; then
-      echo "Skipping ${f} (API_KEY_V2 or MODEL_ID not supplied)"
-      echo
-      continue
-    fi
-  fi
   echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
   echo "${f}"
   echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
   echo
 
-  cat "${f}" > $OUTPUT_FILE
+  sed "s/my-api-key/${MINDEE_API_KEY}/" "${f}" > $OUTPUT_FILE
+  sed -i "s/\/path\/to\/the\/file.ext/src\/test\/resources\/file_types\/pdf\/blank_1.pdf/" $OUTPUT_FILE
 
   if echo "${f}" | grep -q "custom_v1.txt"
   then
@@ -50,18 +43,7 @@ do
     sed -i "s/my-version/1/" $OUTPUT_FILE
   fi
 
-  if echo "${f}" | grep -q "default_v2.txt"
-  then
-    sed -i "s/MY_API_KEY/$API_KEY_V2/" $OUTPUT_FILE
-    sed -i "s/MY_MODEL_ID/$MODEL_ID/" $OUTPUT_FILE
-  else
-    sed -i "s/my-api-key/$API_KEY/" $OUTPUT_FILE
-  fi
-
-  sed -i "s/my-api-key/$API_KEY/" $OUTPUT_FILE
-  sed -i "s/\/path\/to\/the\/file.ext/src\/test\/resources\/file_types\/pdf\/blank_1.pdf/" $OUTPUT_FILE
-
-  javac -cp ./target/dependency/*:./target/* SimpleMindeeClient.java
-  java -cp  .:./target/dependency/*:./target/* SimpleMindeeClient
-
+  sleep 0.5  # avoid too many request errors
+  javac -cp ./target/dependency/*:./target/* "${OUTPUT_FILE}"
+  java -cp  .:./target/dependency/*:./target/* SimpleMindeeClientV1
 done
