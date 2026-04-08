bb386b550421b2739f1fa1df7882874da23b1e74
Ianaré Sévi <ianare@mindee.co>
2026-04-08 11:16:02 +0200

    :recycle: simplify image utilities

--- src/main/java/com/mindee/image/ImageCompressor.java
+++ src/main/java/com/mindee/image/ImageCompressor.java
@@ -1,12 +1,10 @@
 package com.mindee.image;
 
-import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.Iterator;
 import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageWriteParam;
@@ -21,10 +19,10 @@ public final class ImageCompressor {
       Integer newWidth,
       Integer newHeight
   ) {
-    Image scaledImage = inputImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
-    BufferedImage outImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_ARGB);
+    var scaledImage = inputImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+    var outImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_ARGB);
 
-    Graphics2D g2d = outImage.createGraphics();
+    var g2d = outImage.createGraphics();
     g2d.drawImage(scaledImage, 0, 0, null);
     g2d.dispose();
 
@@ -38,8 +36,8 @@ public final class ImageCompressor {
       Integer maxHeight
   ) throws IOException {
 
-    ByteArrayInputStream bis = new ByteArrayInputStream(imageData);
-    BufferedImage original = ImageIO.read(bis);
+    var bis = new ByteArrayInputStream(imageData);
+    var original = ImageIO.read(bis);
     ImageUtils.Dimensions dimensions = ImageUtils
       .calculateNewDimensions(original, maxWidth, maxHeight);
     return compressImage(original, quality, dimensions.width, dimensions.height);
@@ -70,7 +68,7 @@ public final class ImageCompressor {
     if (quality == null) {
       quality = 85;
     }
-    BufferedImage resizedImage = resize(original, finalWidth, finalHeight);
+    var resizedImage = resize(original, finalWidth, finalHeight);
     return encodeToJpegByteArray(resizedImage, (float) quality / 100f);
   }
 
@@ -78,14 +76,14 @@ public final class ImageCompressor {
     if (original.getType() == BufferedImage.TYPE_INT_RGB) {
       return original;
     }
-    BufferedImage newImage = new BufferedImage(
+    var newImage = new BufferedImage(
       original.getWidth(),
       original.getHeight(),
       BufferedImage.TYPE_INT_RGB
     );
-    Graphics2D g = newImage.createGraphics();
-    g.drawImage(original, 0, 0, null);
-    g.dispose();
+    var graphics = newImage.createGraphics();
+    graphics.drawImage(original, 0, 0, null);
+    graphics.dispose();
     return newImage;
   }
 
@@ -93,17 +91,17 @@ public final class ImageCompressor {
       BufferedImage image,
       float quality
   ) throws IOException {
-    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    var outputStream = new ByteArrayOutputStream();
 
-    Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("jpg");
+    var writers = ImageIO.getImageWritersByFormatName("jpg");
     ImageWriter writer = writers.next();
 
-    ImageWriteParam params = writer.getDefaultWriteParam();
+    var params = writer.getDefaultWriteParam();
     params.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
     params.setCompressionQuality(quality);
 
     writer.setOutput(ImageIO.createImageOutputStream(outputStream));
-    BufferedImage alphaCheckedImage = removeAlphaChannel(image);
+    var alphaCheckedImage = removeAlphaChannel(image);
     writer.write(null, new IIOImage(alphaCheckedImage, null, null), params);
     writer.dispose();
 
