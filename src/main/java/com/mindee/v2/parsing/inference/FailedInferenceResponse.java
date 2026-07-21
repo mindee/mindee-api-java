ecad4378a78ec60f30a4fb7277562e7f54a2469d
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2026-07-21 16:47:46 +0200

    :sparkles: add webhook error response (#345)

---
+++ src/main/java/com/mindee/v2/parsing/inference/FailedInferenceResponse.java
@@ -0,0 +1,56 @@
+package com.mindee.v2.parsing.inference;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.mindee.v1.parsing.common.LocalDateTimeDeserializer;
+import com.mindee.v2.parsing.CommonResponse;
+import com.mindee.v2.parsing.error.ErrorResponse;
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Represents an asynchronous polling response.
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class FailedInferenceResponse extends CommonResponse {
+  /**
+   * UUID of the failed inference.
+   */
+  @JsonProperty("inference_id")
+  String inferenceId;
+
+  /**
+   * UUID of the model used.
+   */
+  @JsonProperty("model_id")
+  String modelId;
+
+  /**
+   * Name of the input file.
+   */
+  @JsonProperty("file_name")
+  String fileName;
+
+  /**
+   * Alias sent for the file, if any
+   */
+  @JsonProperty("file_alias")
+  String fileAlias;
+
+  /**
+   * Problem details for the failure, if available.
+   */
+  @JsonProperty("error")
+  ErrorResponse error;
+
+  /**
+   * Date and time when the inference was started.
+   */
+  @JsonProperty("created_at")
+  @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+  LocalDateTime createdAt;
+}
---
+++ src/test/java/com/mindee/v2/parsing/FailedInferenceResponseTest.java
@@ -0,0 +1,34 @@
+package com.mindee.v2.parsing;
+
+import static com.mindee.TestingUtilities.getV2ResourcePath;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.mindee.v2.parsing.inference.FailedInferenceResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("MindeeV2 - Failed Inference Tests")
+public class FailedInferenceResponseTest {
+  @Test
+  @DisplayName("properties must be valid")
+  void whenFailed_mustLoad() throws IOException {
+    var localResponse = new LocalResponse(
+      getV2ResourcePath("errors/webhook_error_500_failed.json")
+    );
+    var response = localResponse.deserializeResponse(FailedInferenceResponse.class);
+    assertNotNull(response);
+    assertEquals("12345678-1234-1234-1234-123456789ABC", response.getInferenceId());
+    assertEquals("default_sample.jpg", response.getFileName());
+    assertEquals("dummy-alias.jpg", response.getFileAlias());
+    assertEquals("dummy-alias.jpg", response.getFileAlias());
+    assertInstanceOf(LocalDateTime.class, response.getCreatedAt());
+    assertNotNull(response.getError());
+    assertNotNull(response.getError());
+    assertEquals(500, response.getError().getStatus());
+    assertEquals("500-012", response.getError().getCode());
+  }
+}
